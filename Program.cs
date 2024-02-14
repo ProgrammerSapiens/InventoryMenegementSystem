6b6538bda7462a596a51070238f8a76c7a9ecb51
Dmitriy <homyakov-dmitriy@mail.ru>
2024-02-14 12:11:59 +0200

    Удаление лишних деректив

--- Program.cs
+++ Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace InventoryMenegementSystem
@@ -16,7 +13,7 @@ namespace InventoryMenegementSystem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new UserForm());
+            Application.Run(new MainForm());
         }
     }
 }
