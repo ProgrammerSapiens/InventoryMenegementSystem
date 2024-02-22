771686b964e63b3e5593fd757c711c10eaa0d21c
Dmitriy <homyakov-dmitriy@mail.ru>
2024-02-22 08:41:37 +0200

    Создание форм Customer заново

---
+++ CustomerForm.cs
@@ -0,0 +1,71 @@
+﻿using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace InventoryMenegementSystem
+{
+    public partial class CustomerForm : Form
+    {
+        SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\PotatoKiller\Documents\dbIMS.mdf;Integrated Security=True;Connect Timeout=30");
+        SqlCommand cmd = new SqlCommand();
+        SqlDataReader dr;
+
+        public CustomerForm()
+        {
+            InitializeComponent();
+            LoadCustomer();
+        }
+
+        public void LoadCustomer()
+        {
+            int i = 0;
+            dgvCustomer.Rows.Clear();
+            cmd = new SqlCommand("SELECT * FROM tbCustomer", conn);
+            conn.Open();
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                i++;
+                dgvCustomer.Rows.Add(i, dr[0].ToString(), dr[1].ToString(), dr[2].ToString());
+            }
+            dr.Close();
+            conn.Close();
+        }
+
+        private void btnAdd_Click(object sender, System.EventArgs e)
+        {
+            CustomerModuleForm customerModule = new CustomerModuleForm();
+            customerModule.btnSave.Enabled = true;
+            customerModule.btnUpdate.Enabled = false;
+            customerModule.ShowDialog();
+            LoadCustomer();
+        }
+
+        private void dgvCustomer_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            string colName = dgvCustomer.Columns[e.ColumnIndex].Name;
+            if (colName == "Edit")
+            {
+                CustomerModuleForm customerModule = new CustomerModuleForm();
+                customerModule.lblCId.Text = dgvCustomer.Rows[e.RowIndex].Cells[1].Value.ToString();
+                customerModule.txtCName.Text = dgvCustomer.Rows[e.RowIndex].Cells[2].Value.ToString();
+                customerModule.txtCPhone.Text = dgvCustomer.Rows[e.RowIndex].Cells[3].Value.ToString();
+
+                customerModule.btnSave.Enabled = false;
+                customerModule.btnUpdate.Enabled = true;
+                customerModule.ShowDialog();
+            }
+            else if (colName == "Delete")
+            {
+                if (MessageBox.Show("Are you sure you want to delete this customer?", "Delete Record", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    conn.Open();
+                    cmd = new SqlCommand("DELETE FROM tbCustomer WHERE cId LIKE '" + dgvCustomer.Rows[e.RowIndex].Cells[1].Value.ToString() + "'", conn);
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                    MessageBox.Show("Record has been successfully deleted!");
+                }
+            }
+            LoadCustomer();
+        }
+    }
+}
---
+++ CustomerModuleForm.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace InventoryMenegementSystem
+{
+    public partial class CustomerModuleForm : Form
+    {
+        SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\PotatoKiller\Documents\dbIMS.mdf;Integrated Security=True;Connect Timeout=30");
+        SqlCommand cmd = new SqlCommand();
+
+        public CustomerModuleForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (MessageBox.Show("Are you sure you want to save this customer?", "Saving Record", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    cmd = new SqlCommand("INSERT INTO tbCustomer (cname, cphone) VALUES(@cname, @cphone)", conn);
+                    cmd.Parameters.AddWithValue("@cname", txtCName.Text);
+                    cmd.Parameters.AddWithValue("@cphone", txtCPhone.Text);
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                    MessageBox.Show("Customer has been successfully saved");
+                    this.Dispose();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public void Clear()
+        {
+            txtCName.Clear();
+            txtCPhone.Clear();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (MessageBox.Show("Are you sure you want to update this customer?", "Update Record", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    cmd = new SqlCommand("UPDATE tbCustomer SET cname=@cname, cphone=@cphone WHERE cId LIKE'" + lblCId.Text + "'", conn);
+                    cmd.Parameters.AddWithValue("@cname", txtCName.Text);
+                    cmd.Parameters.AddWithValue("@cphone", txtCPhone.Text);
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                    MessageBox.Show("Customer has been successfully updated");
+                    Clear();
+                    this.Dispose();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            Clear();
+            btnSave.Enabled = true;
+            btnUpdate.Enabled = false;
+        }
+
+        private void pictureBoxClose_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+    }
+}
